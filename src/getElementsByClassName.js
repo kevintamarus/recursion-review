c58622dc9a0a91b42a37108d7a722c53330f8c4d
Hack Reactor Student <student@hackreactor.com>
2017-06-26 15:45:43 -0700

    trying to figure out getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,22 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
+ // document.body, element.childNodes, and element.classList
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+//input => class name(string)
+//output => every element in the class name in an array ex.: []
+     //ex: find className "prices" it will return => ["$259","$300", "$400"]
+var getElementsByClassName = function(className) {
+  
+  var returnArray = [];
+//Search main node for elements with className
+//iterate over child nodes, check their class attributes
+  //If there elements with the same classname attribute, push to an array.
+  for(var i = 0; i < document.body.classList.length; i++) {
+    if (document.body.classList[i] === className) {
+      returnArray.push(1);
+    }
+  }
+  return returnArray;
 };
