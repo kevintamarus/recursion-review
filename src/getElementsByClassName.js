bcd2ec8a3e9cc92b274e520b570e3936a98cea62
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-06-27 08:56:34 -0700

    getElementsByClassName passed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -9,15 +9,20 @@
 //output => every element in the class name in an array ex.: []
      //ex: find className "prices" it will return => ["$259","$300", "$400"]
 var getElementsByClassName = function(className) {
-  
-  var returnArray = [];
-//Search main node for elements with className
-//iterate over child nodes, check their class attributes
-  //If there elements with the same classname attribute, push to an array.
-  for(var i = 0; i < document.body.classList.length; i++) {
-    if (document.body.classList[i] === className) {
-      returnArray.push(1);
+  var newArray = [];
+  var element = document.body;
+  checkElement(element);
+  function checkElement(node) {
+    if (node.classList.contains(className)) {
+        newArray.push(node);
+    }
+    if (node.childNodes) {
+      for(var i=0; i < node.childNodes.length; i++) {
+        if (node.childNodes[i].nodeType !== Node.TEXT_NODE) {
+          checkElement(node.childNodes[i]);
+        }
+      }
     }
   }
-  return returnArray;
+  return newArray;
 };
